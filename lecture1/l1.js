ce8a59d8396749034f79ca90c439f3134a9caa79
Yuliia Ostrovska <ostrovskajuli@gmai.com>
2023-10-29 13:46:55 +0200

    Example 1

---
+++ lecture1/l1.js
@@ -0,0 +1,14 @@
+console.log("Задачі по JS");
+console.log(
+  "https://github.com/goitacademy/js-instructor-examples/blob/main/lesson-01/uk.md"
+);
+console.log(
+  "Example 1 - Математичні оператори. Виведи на екран загальну кількість яблук та винограду. Різницю яблук та винограду."
+);
+
+const apples = 47;
+const grapes = 135;
+const total = apples + grapes;
+console.log(total);
+const diff = grapes - apples;
+console.log(diff);
